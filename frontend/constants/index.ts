7104779a8c3756ad717eca6d4c3c6a7c104c889e
kimthu09 <21521495@gm.uit.edu.vn>
2024-03-09 12:43:16 +0700

    dev: layout

---
+++ frontend/constants/index.ts
@@ -0,0 +1,30 @@
+export type SidebarItem = {
+  title: string;
+  href: string;
+};
+export const sidebarItems: SidebarItem[] = [
+  {
+    title: "Trang chủ",
+    href: "/",
+  },
+  {
+    title: "Hợp đồng",
+    href: "/contract",
+  },
+  {
+    title: "Tài liệu",
+    href: "/documents",
+  },
+  {
+    title: "Đồ án",
+    href: "/project",
+  },
+  {
+    title: "Liên hệ",
+    href: "/contact",
+  },
+  {
+    title: "Quy tắc",
+    href: "/rule",
+  },
+];
--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -1,6 +1,13 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+export function on(obj: any, ...args: any) {
+  obj.addEventListener(...args);
+}
+
+export function off(obj: any, ...args: any) {
+  obj.removeEventListener(...args);
 }
