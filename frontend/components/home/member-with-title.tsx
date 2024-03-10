ebe801352809a06d550db2d1ccc45c364a15aa34
NLNM-0-0 <nguyenlengocmai000@gmail.com>
2024-03-11 02:47:22 +0700

    dev: add member to home page update: change color of table in contract page

---
+++ frontend/components/home/member-with-title.tsx
@@ -0,0 +1,12 @@
+import Member from "../user/member";
+
+const MemberWithTitle = () => {
+    return (
+        <div className="w-full flex-col items-center">
+            <h2 className="text-center text-4xl pb-16">Thành viên nhóm</h2>
+            <Member />
+        </div>
+    )
+}
+
+export default MemberWithTitle;
