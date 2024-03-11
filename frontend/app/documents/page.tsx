1d468f22d8eb70693ac10ff7b3510b65a8b0d6bd
NLNM-0-0 <nguyenlengocmai000@gmail.com>
2024-03-11 14:55:10 +0700

    update: pdf height

--- frontend/app/documents/page.tsx
+++ frontend/app/documents/page.tsx
@@ -16,8 +16,8 @@ const DocumentsPage = () => {
                                 <AccordionTrigger className="text-xl backdrop-blur-sm bg-white/50 px-4 rounded">{item.title}</AccordionTrigger>
                                 <AccordionContent>
                                     <div className="flex flex-col gap-8 lg:flex-row lg:gap-0 mb-8">
-                                        <iframe className="flex-1 h-[500px]" src={item.bienBanPdfPath} />
-                                        <iframe className="flex-1 h-[500px]" src={item.phanCongPdfPath} />
+                                        <iframe className="flex-1" height={800} src={item.bienBanPdfPath} />
+                                        <iframe className="flex-1" height={800} src={item.phanCongPdfPath} />
                                     </div>
                                 </AccordionContent>
                             </AccordionItem>
--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="w-full fixed bottom-0 h-7 bg-primary flex justify-center items-center text-white">
-      &copy; 2023 by Nhom5. GV: ThS. Tạ Việt Phương
+      &copy; 2024 by Nhom5. GV: ThS. Tạ Việt Phương
     </div>
   );
 };
