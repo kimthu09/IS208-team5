79a5d60c2496e8a3ad4e7223ca36afcacf257e6b
NLNM-0-0 <nguyenlengocmai000@gmail.com>
2024-03-18 00:43:58 +0700

    update: group intro

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,15 +1,13 @@
 import GroupIntroduction from "@/components/home/group-introduction";
 import Introduce from "@/components/home/introduce";
-import MemberWithTitle from "@/components/home/member-with-title";
 import Tool from "@/components/home/tool";
 
 export default function Home() {
   return (
     <div className="flex md:mt-32 mt-20 flex-col gap-32">
       <Introduce />
-      <Tool />
       <GroupIntroduction />
-      <MemberWithTitle />
+      <Tool />
     </div>
   );
 }
